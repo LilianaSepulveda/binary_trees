adcfe7619899be836f2a4e8224ab8db6188b3216
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-07-21 19:24:54 -0500

    fix identation,function that measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,15 +11,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height = 0, h_left = 0, h_right = 0;
 
-if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-
 	h_left = binary_tree_height(tree->left);
 	h_right = binary_tree_height(tree->right);
 
-if (h_left > h_right)
+	if (h_left > h_right)
 		height = h_left;
-else
+	else
 		height = h_right;
-return (height + 1);
+	return (height + 1);
 }
